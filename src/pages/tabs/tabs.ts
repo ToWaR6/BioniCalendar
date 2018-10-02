fcd2f5da63cce414800f591c872ec4b6e2f6b7bc
ToWaR6 <loic.crauser@free.fr>
2018-10-02 12:44:37 +0200

    notif

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { NotificationsPage } from '../notifications/notifications';
+import { AddeventPage } from '../addevent/addevent';
+import { SearchPage } from '../search/search';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = SearchPage;
+  tab2Root = AddeventPage;
+  tab3Root = NotificationsPage;
 
   constructor() {
 
